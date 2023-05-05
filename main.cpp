69d65d72d3c8f955c640a85b76e97d1016800ea5
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-05 18:23:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int a=1, b=0, c=0, n;
+
+    cin >> n;
+
+    while( c<n){
+        c=a+b;
+        if(c<=n){
+            cout << c << endl;
+        }
+        a=b;
+        b=c;
+    }
    return 0;
 }
